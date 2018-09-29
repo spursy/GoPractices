a518b637e2dfa5e9877298f9352029e445b319c4
spursyy <email@example.com>
2018-09-29 18:52:51 +0800

    update

---
+++ GoPractice/chapter1/command.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input your name:")
+	input, err := inputReader.ReadString('\n')
+	
+	if err != nil {
+		fmt.Printf("Found an error: %s\n", err)
+	} else {
+		input = input[:len(input) - 1]
+		fmt.Printf("Hello, %s\n", input)
+	}
+}
